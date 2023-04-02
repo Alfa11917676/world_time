37f6ae128afbd91ba12db53bbb586d1f08280ee9
0xAnyx <97167227+0xAlfa@users.noreply.github.com>
2023-04-02 11:42:07 +0530

    Initial commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() {
+    // simulate network request for a username
+    Future.delayed(const Duration(seconds: 3), () {
+      print('yoshi');
+    });
+    print('getting data');
+
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('function ran');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build function ran');
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('Counter is $counter'),
+      )
+    );
+  }
+}
